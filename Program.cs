b5f0fcec84a4bc8fc01bb2f5faca789778e61e1b
kanchanCode <jkanchan2911@gmail.com>
2021-10-21 16:30:04 +0530

    UserCases_Q2

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_4_Assignment2
+{
+    public class UC1
+    { 
+    public double x1, y1, x2, y2;
+    
+        public UC1(double x1,double y1,double x2,double y2)
+    {
+            this.x1=x1;
+            this.y1=y1;
+            this.x2=x2;
+            this.y2=y2;
+    }
+        public double compute()
+        {
+        double x = 0;
+        double y = 0;
+        double a = 0;
+        double b = 0;
+        double length = 0;
+
+           //Console.WriteLine("Enter value for x1");
+            //x1 = Convert.ToDouble(Console.ReadLine());
+           // Console.WriteLine("Enter value for y1");
+            //y1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter value for x2");
+           // x2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter value for y2");
+           // y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("First point (" + x1 + "," + y1 + ")");
+            Console.WriteLine("Second point (" + x2 + "," + y2 + ")");
+
+            x = x2 - x1;
+            y = y2 - y1;
+
+            a = Math.Pow(x, 2);
+            b = Math.Pow(y, 2);
+
+            length = Math.Sqrt(a + b);
+
+            Console.WriteLine("Length of the line is " + length);
+
+            return length;
+
+           
+
+        }
+    }
+}
---
+++ objects.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_4_Assignment2
+{
+    class Program
+
+    {
+     static void Main(string[] args)
+        {
+
+            //User case 1
+            UC1 Line1 = new UC1(3, 4, 7,8);
+            double j=Line1.compute();
+            UC1 Line2 = new UC1(30, 4, 17, 8);
+            double k = Line2.compute();
+
+            //UserCase 2
+            if (j == k)
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
+
+            //UserCase 3
+            if (j>k)
+            {
+                Console.WriteLine("Length of Line 1 is greater than Line 2");
+            }
+            else if (j<k)
+            {
+                Console.WriteLine("Length of Line 1 is smaller than Line 2");
+
+            }
+        }
+       
+    
+    }
+}
