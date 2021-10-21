3f916086608d657f49e0daf67a88f22da395810c
kanchanCode <jkanchan2911@gmail.com>
2021-10-21 16:20:21 +0530

    UserCases_Q2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_4_Assignment2
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            UC1 line1 = new UC1(3, 4, 7, 8);
+            line1.compute();
+            UC1 line2 = new UC1(10, 30, 20, 5);
+            line2.compute();
+
+        }
+
+    }
+}
+
+
